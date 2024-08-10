17ba3375e5478d9219f4b9e02f47f235df52e111
Mikaela Purugganan <mikaela.programming@gmail.com>
2024-08-10 22:57:44 +0800

    create seekbar functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,8 +3,18 @@ import { assets } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { track, seekBar, seekBg, playStatus, play, pause, time } =
-    useContext(PlayerContext);
+  const {
+    track,
+    seekBar,
+    seekBg,
+    playStatus,
+    play,
+    pause,
+    time,
+    previous,
+    next,
+    seekSong,
+  } = useContext(PlayerContext);
 
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
@@ -22,7 +32,12 @@ const Player = () => {
             src={assets.shuffle_icon}
             alt=''
           />
-          <img className='w-4 cursor-pointer' src={assets.prev_icon} alt='' />
+          <img
+            onClick={previous}
+            className='w-4 cursor-pointer'
+            src={assets.prev_icon}
+            alt=''
+          />
           {playStatus ? (
             <img
               onClick={pause}
@@ -39,7 +54,12 @@ const Player = () => {
             />
           )}
 
-          <img className='w-4 cursor-pointer' src={assets.next_icon} alt='' />
+          <img
+            onClick={next}
+            className='w-4 cursor-pointer'
+            src={assets.next_icon}
+            alt=''
+          />
           <img className='w-4 cursor-pointer' src={assets.loop_icon} alt='' />
         </div>
         <div className='flex items-center gap-5'>
@@ -48,6 +68,7 @@ const Player = () => {
           </p>
           <div
             ref={seekBg}
+            onClick={seekSong}
             className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'
           >
             <hr
